17b54a016af10a7511a097dc6da7cad59f6828a3
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-20 23:38:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,5 @@
 def countdown(number)
   while number > 0
-   sleep 1
     puts "#{number} SECOND(S)!"
     number -= 1
   end
@@ -11,7 +10,6 @@ countdown(10)
 
 def countdown_with_sleep(number)
   while number > 0
-   sleep 1
     puts "#{number} SECOND(S)!"
     number -= 1
   end
